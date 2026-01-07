4676ed42771b405676ad1520a79ec607ab909b1e
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2026-01-06 21:25:07 -0500

    mroe changes

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -3,20 +3,38 @@ import requests
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-def get_chat_response(message: str, mode: str):
-    """
-    Sends the user's message to OpenAI's ChatGPT API and returns the AI response.
-    """
+def get_chat_response(message: str, model: str):
+    # Creat the API request
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Authorization": f"Bearer {OPENAI_API_KEY}",
         "Content-Type": "application/json"
     }
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": message}],
-        "temperature": 0.7
-    }
+
+    # Logic to choose data json input
+    if (model=="chatgpt-5-mini"):
+        data = {
+            "model": "gpt-5-mini",
+            "messages": [{"role": "user", "content": message}],
+            "temperature": 0.7
+        }
+    if (model=="chatgpt-5"):
+        data = {
+            "model": "gpt-5",
+            "messages": [{"role": "user", "content": message}],
+            "temperature": 0.7
+        }
+    elif (model=="imagegen"):
+        data = {
+            "model": "gpt-image-1.5",
+            "messages": [{"role": "user", "content": message}],
+            "temperature": 0.7
+        }
+    
+    # Inject Data into SQL DB
+
+
+    # Run the 7 day check
 
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
