3356ba89a03a514698f6da2fc087fc96379117e5
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2026-01-10 13:00:13 -0500

    Update login.py

--- app/routes/login.py
+++ app/routes/login.py
@@ -7,9 +7,8 @@ from app.services.login_service import authenticate
 login_bp = Blueprint("login", __name__)
 @login_bp.route("/login", methods=["GET", "POST"])
 def login_screen():
-
     # If the user is logged in, redirect them to chat
-    if 'logged_in' in session and not session['logged_in']:
+    if 'logged_in' in session and session['logged_in']:
         return redirect(url_for('chat.chat_screen'))
     
     # On form submit
